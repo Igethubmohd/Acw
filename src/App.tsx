295c4e0b5c7ec9d271488471b804e26e18665542
Igethubmohd <147734350+Igethubmohd@users.noreply.github.com>
2024-11-13 00:37:53 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>ACW</h1>
+      <h2> Bismilah:Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
